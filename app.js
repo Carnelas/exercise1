f6acfdb85d30cb7ca59572b5634f5c75e0b995b4
Carnelas <carnelas@gmail.com>
2018-10-31 09:10:05 +0100

    añadido validador

--- app.js
+++ app.js
@@ -8,21 +8,32 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.post('/message',  (req, res, next) =>{
-  //coge del body el destinatario y el mensaje
-  const {destination, body} = req.body;
-  //se sustituye localhost por messageapp porque es lo que tenemos en dockerfile
-  axios.post('http://messageapp:3000/message', {destination, body})
-    .then(resp => {
-      res.status(200).
-      res.send(`${resp.data}`)
-    })
-    .catch(e => {
-      res.status(500).
-      console.log(e)})
-  })
 
-  
-  app.listen(9001, () => {
-    console.log('listend on port 9001!');
-  });
+const test = (req, res, next) => {
+  let { destination, body } = req.body;
+  if (typeof destination != "string" || typeof body != "string"
+    || destination == "" || body == "" || body.length > 240
+    || destination.length > 12 || destination == null || body == null
+    || destination == undefined || body == undefined) {
+    console.log("Error")
+    res.status(400).send("Revisa lo que has escrito, algo falla")
+  } else next();
+
+
+  app.post('/message', (req, res, next) => {
+    let { destination, body } = req.body;
+    axios.post('http://messageapp:3000/message', { destination, body })
+      .then(resp => {
+        res.status(200).
+          res.send(`${resp.data}`)
+      })
+      .catch(e => {
+        res.status(500).
+          console.log(e)
+      })
+  })
+}
+
+app.listen(9001, () => {
+  console.log('listend on port 9001!');
+});
